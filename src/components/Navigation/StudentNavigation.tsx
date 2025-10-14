af1560fcefbd466ed5a08d4cfaa0f0651515a957
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-10-14 17:32:31 +0300

    feat: Added a student navigation

---
+++ src/components/Navigation/StudentNavigation.tsx
@@ -0,0 +1,40 @@
+import React, {  } from "react";
+import { Link } from "react-router-dom";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { UserOutlined } from "@ant-design/icons";
+import { LogOut } from "lucide-react";
+
+const StudentNavigation: React.FC = () => {
+
+    return(
+        <div className="z-50 fixed w-full px-4 top-0 left-0 flex justify-between items-center bg-second-custom text-center">
+            <Link to="/student/home" className="font-extrabold">E-exam</Link>
+            <div className="flex gap-8 items-center font-medium ">
+                <Link to="/" className="border-b-4 py-4 border-b-white text-white transition-all duration-300">Accueil</Link>
+                <Link to="/" className="border-b-4 border-b-transparent py-4 hover:border-b-white hover:text-white transition-all duration-300">Annonce</Link>
+                <Link to="/" className="border-b-4 border-b-transparent py-4 hover:border-b-white hover:text-white transition-all duration-300">Test</Link>
+                <Link to="/" className="border-b-4 border-b-transparent py-4 hover:border-b-white hover:text-white transition-all duration-300">Resultat</Link>
+            </div>
+            <Popover>
+                <PopoverTrigger asChild>
+                <Button
+                    variant={"outline"}
+                >
+                    <UserOutlined />
+                    antsafider@gmail.com
+                </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-4 mr-4">
+                    <div className="mb-2 text-gray-700 font-medium">Menu</div>
+                    <div className="w-52 flex flex-col gap-2">
+                        <Button variant={'outline'}><UserOutlined /> Profile</Button>
+                        <Button variant={'outline'}><LogOut /> Deconnecter</Button>
+                    </div>
+                </PopoverContent>
+            </Popover>  
+        </div>
+    )
+}
+
+export default StudentNavigation;
