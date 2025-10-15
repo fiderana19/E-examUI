5f67a831e6200d0a51a8888264ecd66145228e30
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-10-15 19:46:57 +0300

    feat: Fixed the admin nav active location

--- src/components/Navigation/AdminNavigation.tsx
+++ src/components/Navigation/AdminNavigation.tsx
@@ -1,23 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Button } from "../ui/button";
 import { LogOut } from "lucide-react";
 import { BookOutlined, HomeOutlined, UserSwitchOutlined } from "@ant-design/icons";
 
 const AdminNavigation: React.FC = () => {
+    const location = useLocation();
 
     return(
         <div className="z-50 w-60 fixed top-0 left-0 p-4 flex flex-col justify-between h-screen bg-second-custom">
             <Link  to="/student/home" className="font-extrabold text-center">E-exam</Link>
             <div className="flex flex-col gap-2 font-medium w-full">
                 <Link to="/admin/home">
-                    <Button className="w-full" variant={'secondary'}><HomeOutlined /> Accueil</Button>
+                    <Button className="w-full" variant={location.pathname === "/admin/home" ? 'secondary' : 'ghost'}><HomeOutlined /> Accueil</Button>
                 </Link>
                 <Link to="/admin/groupe">
-                    <Button className="w-full" variant={'ghost'}><BookOutlined /> Groupe</Button>
+                    <Button className="w-full" variant={location.pathname === "/admin/groupe" ? 'secondary' : 'ghost'}><BookOutlined /> Groupe</Button>
                 </Link>
                 <Link to="/admin/account">
-                    <Button className="w-full" variant={'ghost'}><UserSwitchOutlined /> Compte</Button>
+                    <Button className="w-full" variant={location.pathname === "/admin/account" ? 'secondary' : 'ghost'}><UserSwitchOutlined /> Compte</Button>
                 </Link>
             </div>
             <div className="w-52 flex flex-col gap-2">
