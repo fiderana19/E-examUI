dfeefed641e697e1281c732f28871df9ac58b35e
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-10-17 14:28:44 +0300

    feat: Fixed the student result skeleton

--- src/pages/student/StudentResult.tsx
+++ src/pages/student/StudentResult.tsx
@@ -1,5 +1,6 @@
 import StudentNavigation from "@/components/Navigation/StudentNavigation";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { ClockCircleOutlined, CloseOutlined, FilePdfOutlined } from "@ant-design/icons";
 import React from "react";
 
@@ -13,35 +14,43 @@ const StudentResult: React.FC  = () => {
                 <CloseOutlined className="text-7xl" />
                 <div className="mt-4 text-xl">Aucun resultat disponible.</div>
             </div>
-            <div className="">
-                <div className="shadow p-4 bg-white">
-                    <div className="mb-4">
+            <div className="my-6">
+                <Card className="px-4 my-1 cursor-pointer">
+                    <div>
+                        <div className="text-xs text-gray-600">Publié le 2025-12-12 08:00</div>
                         <div className="flex justify-between">
-                            <div className="flex gap-2">
-                                <div className="font-bold text-lg">Test de HTML du </div>
-                                <div className="text-gray-600 font-bold text-lg">
-                                    2025-10-25 15:00
-                                </div>
-                            </div>
-                            <div className="font-bold text-gray-800">Mr Andry</div>
-                        </div>
-                        <div className="flex gap-2 my-1">
-                                <div className="border rounded-full px-2 bg-gray-400 text-white">
-                                    <ClockCircleOutlined /> 45:00
-                                </div>
-                            <div>
-                                Heure de soumission : 2025-10-25 15:45
-                            </div>
-                        </div>                        
-                        <div className="text-gray-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, expedita quidem non sint esse perferendis ex exercitationem molestias cum</div>
-                        <div className="flex justify-end">
-                            <div>Note: 19/20</div>
+                            <div className="font-medium">M1 | vuefnbv</div>
+                            <div className="text-gray-700 font-bold"><FilePdfOutlined className=" mr-2" />result.pdf</div>
                         </div>
                     </div>
-                    <div className="flex justify-end">
-                        <Button><FilePdfOutlined /> Generer un relevé</Button>
+                </Card>
+                <Card className="px-4 my-1 cursor-pointer">
+                    <div>
+                        <div className="text-xs text-gray-600">Publié le 2025-12-12 08:00</div>
+                        <div className="flex justify-between">
+                            <div className="font-medium">M1 | vuefnbv</div>
+                            <div className="text-gray-700 font-bold"><FilePdfOutlined className=" mr-2" />result.pdf</div>
+                        </div>
                     </div>
-                </div>
+                </Card>
+                <Card className="px-4 my-1 cursor-pointer">
+                    <div>
+                        <div className="text-xs text-gray-600">Publié le 2025-12-12 08:00</div>
+                        <div className="flex justify-between">
+                            <div className="font-medium">M1 | vuefnbv</div>
+                            <div className="text-gray-700 font-bold"><FilePdfOutlined className=" mr-2" />result.pdf</div>
+                        </div>
+                    </div>
+                </Card>
+                <Card className="px-4 my-1 cursor-pointer">
+                    <div>
+                        <div className="text-xs text-gray-600">Publié le 2025-12-12 08:00</div>
+                        <div className="flex justify-between">
+                            <div className="font-medium">M1 | vuefnbv</div>
+                            <div className="text-gray-700 font-bold"><FilePdfOutlined className=" mr-2" />result.pdf</div>
+                        </div>
+                    </div>
+                </Card>
             </div>
         </div>
     </div>
