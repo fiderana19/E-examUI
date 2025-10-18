487cb30ee58717b07c86f43badb275cf746515c4
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-10-18 10:41:00 +0300

    feat: Fixed the annonce hooks

---
+++ src/hooks/annonce/useDeleteAnnonce.ts
@@ -0,0 +1,28 @@
+import { deleteAnnonce } from "@/api/annonce.api"
+import { TOAST_TYPE } from "@/constants/ToastType"
+import { showToast } from "@/utils/Toast"
+import { useMutation } from "@tanstack/react-query"
+import { AxiosError } from "axios"
+
+export const useDeleteAnnonce = ({action} : {action: () => void}) => {
+    const mutation = useMutation({
+        mutationFn: (data: number) => deleteAnnonce(data),
+        onSuccess: () => {
+            if(action) {
+                action();
+            }
+            showToast({
+                type: TOAST_TYPE.SUCCESS,
+                message: "Annonce supprimée !"
+            })
+        },
+        onError: (error: AxiosError) => {
+            showToast({
+                type: TOAST_TYPE.ERROR,
+                message: "Erreur lors de la suppression de l'annonce !"
+            })
+        }
+    })
+
+    return mutation;
+}
---
+++ src/hooks/annonce/usePostAnnonce.ts
@@ -0,0 +1,29 @@
+import { postAnnonce } from "@/api/annonce.api"
+import { TOAST_TYPE } from "@/constants/ToastType"
+import { AnnounceAddInterface } from "@/interfaces/announce.interface"
+import { showToast } from "@/utils/Toast"
+import { useMutation } from "@tanstack/react-query"
+import { AxiosError } from "axios"
+
+export const usePostAnnonce = ({action} : {action: () => void}) => {
+    const mutation = useMutation({
+        mutationFn: (data: AnnounceAddInterface) => postAnnonce(data),
+        onSuccess: () => {
+            if(action) {
+                action();
+            }
+            showToast({
+                type: TOAST_TYPE.SUCCESS,
+                message: "Annonce publiée !"
+            })
+        },
+        onError: (error: AxiosError) => {
+            showToast({
+                type: TOAST_TYPE.ERROR,
+                message: "Erreur lors de la publication de l'annonce !"
+            })
+        }
+    })
+
+    return mutation;
+}
