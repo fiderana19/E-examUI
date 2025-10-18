64ab7cdb40e5477f9561d7b85fdffbf02ab22ab0
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-10-18 11:09:53 +0300

    feat: Fixed the result hooks

---
+++ src/hooks/result/useGetResultByGroupId.ts
@@ -0,0 +1,30 @@
+import { QueryCacheKey } from "@/api/QueryCacheKey"
+import { getResultByGroupId } from "@/api/result.api"
+import { TOAST_TYPE } from "@/constants/ToastType"
+import { showToast } from "@/utils/Toast"
+import { useQuery } from "@tanstack/react-query"
+import { useEffect } from "react"
+
+export const useGetResultByGroupId = (id: number) => {
+    const { data, isLoading, isError, error ,refetch } = useQuery({
+        queryKey: [QueryCacheKey.RESULTS, id],
+        queryFn: () => getResultByGroupId(id),
+        staleTime: Infinity,
+        enabled: id !== 0
+    })
+
+    useEffect(() => {
+        if(isError) {
+            showToast({
+                type: TOAST_TYPE.ERROR,
+                message: "Erreur lors de la recuperations des resultats !"
+            })
+        }
+    }, [error])
+
+    return {
+        data: data?.data,
+        isLoading,
+        refetch
+    }
+}
