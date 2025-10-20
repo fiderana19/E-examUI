fdaaf364a7d3e88f72baf2d4c5bdd499e35bdd5f
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-10-20 13:35:13 +0300

    feat: Fixed the password visibility in login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,21 +3,27 @@ import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Controller, useForm } from "react-hook-form";
-import React from "react";
+import React, { useState } from "react";
 import { yupResolver } from '@hookform/resolvers/yup'
 import { LoginInterface } from "@/interfaces/user.interface";
 import { LoginValidation } from "@/validation/user.validation";
 import { Link } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
+import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 
 const LoginPage: React.FC  = () => {
     const { login } = useAuth();
+    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
     const { handleSubmit: submit, formState: { errors }, control } = useForm<LoginInterface>({
         resolver: yupResolver(LoginValidation)
     })
 
     const handleSubmit = async (data: LoginInterface) => {
         await login(data);
+    }
+
+    const handlePasswordVisible = async () => {
+        setIsPasswordVisible(!isPasswordVisible);
     }
 
     return <div className="flex min-h-screen flex-col justify-center">
@@ -35,14 +41,28 @@ const LoginPage: React.FC  = () => {
                         )}
                     />
                     { errors?.email && <div className="text-xs w-full text-red-500 text-left">{ errors?.email.message }</div> }
-                    <Label className="mb-1 mt-4">Mot de passe :</Label>
-                    <Controller
-                        control={control}
-                        name="motdepasse"
-                        render={({ field: { value, onChange } }) => (
-                            <Input value={value} onChange={onChange} className={`${errors?.motdepasse && 'border border-red-500 text-red-500 rounded'}`} />
-                        )}
-                    />
+                            <Label htmlFor='password' className='mb-1 mt-4'>Mot de passe :</Label>
+                            <div className='relative'>
+                                <Controller 
+                                    control={control}
+                                    name='motdepasse'
+                                    render={({
+                                        field: { value, onChange, onBlur }
+                                    }) => (
+                                        <Input value={value} onChange={onChange} onBlur={onBlur} type={!!(isPasswordVisible) ? 'text' : 'password'} className={`${errors?.motdepasse && 'border border-red-500 text-red-500 rounded'}`} />
+                                    )}
+                                />
+                                {
+                                    isPasswordVisible ?
+                                    <EyeInvisibleOutlined 
+                                        onClick={handlePasswordVisible}
+                                        className='absolute top-1.5 right-1.5 cursor-pointer p-1.5' />
+                                    : 
+                                    <EyeOutlined 
+                                        onClick={handlePasswordVisible}
+                                        className='absolute top-1.5 right-1.5 cursor-pointer p-1.5' />
+                                }
+                            </div>
                     { errors?.motdepasse && <div className="text-xs w-full text-red-500 text-left">{ errors?.motdepasse.message }</div> }
                     <div className="flex justify-center mt-4">
                         <Button type="submit">Se connecter</Button>
