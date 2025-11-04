0f3b273111b9b331c73afd94c577ecb649602f51
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-11-04 22:55:18 +0300

    feat: Fixed and created some interfaces

--- src/interfaces/response.interface.ts
+++ src/interfaces/response.interface.ts
@@ -2,3 +2,15 @@ export interface GivePointsInterface {
   id_reponse: string;
   score_question: number;
 }
+
+export interface CreateResponseInterface {
+  id_test: string;
+  id_tentative: string;
+  reponses: ResponseInterface[]
+}
+
+export interface ResponseInterface {
+  id_tentative: string;
+  id_question: string;
+  reponse_texte: string;
+}
