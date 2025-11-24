71a85d83c54db776529e88685a4ce8ae98dd45f7
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-11-24 12:28:24 +0300

    feat: Fixed the lazy loading in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,249 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import StudentHome from "./pages/student/StudentHome";
-import StudentAnnonce from "./pages/student/StudentAnnonce";
-import StudentTest from "./pages/student/StudentTest";
-import StudentProfile from "./pages/student/StudentProfile";
-import StudentResult from "./pages/student/StudentResult";
-import TestRoom from "./pages/student/ExamenSpace/TestRoom";
-import AdminHome from "./pages/admin/AdminHome";
-import AdminGroupe from "./pages/admin/AdminGroupe";
-import AdminGroupeEdit from "./pages/admin/groupe/AdminGroupeEdit";
-import AdminAccount from "./pages/admin/AdminAccount";
-import TeacherHome from "./pages/teacher/TeacherHome";
-import TeacherAnnounce from "./pages/teacher/TeacherAnnounce";
-import TeacherAnnounceEdit from "./pages/teacher/annonce/TeacherAnnonceEdit";
-import TeacherTest from "./pages/teacher/TeacherTest";
-import TeacherTestAdd from "./pages/teacher/test/TeacherTestAdd";
-import TeacherTestEdit from "./pages/teacher/test/TeacherTestEdit";
-import TeacherTestView from "./pages/teacher/test/TeacherTestView";
-import TeacherQCMView from "./pages/teacher/test/TeacherQCMView";
-import TeacherQuestionEdit from "./pages/teacher/test/TeacherQuestionEdit";
-import TeacherCorrection from "./pages/teacher/TeacherCorrection";
-import TeacherCorrectionView from "./pages/teacher/correction/TeacherCorrectionView";
-import TeacherCorrectionAction from "./pages/teacher/correction/TeacherCorrectionAction";
-import TeacherResult from "./pages/teacher/TeacherResult";
-import TeacherResultView from "./pages/teacher/result/TeacherResultView";
-import TeacherProfile from "./pages/teacher/TeacherProfile";
-import AdminHistory from "./pages/admin/AdminHistory";
-import AdminHistoryView from "./pages/admin/history/AdminHistoryView";
-import AdminResponseView from "./pages/admin/history/AdminResponseView";
-import AdminResult from "./pages/admin/AdminResult";
-import RoleBasedRoute from "./routes/RoleBasedRoute";
-import TeacherTestLaunchedView from "./pages/teacher/test/TeacherTestLaunchedView";
-import NotFound from "./pages/NotFound";
-import Unauthorized from "./pages/Unauthorized";
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const SignupPage = lazy(() => import("./pages/SignupPage"));
+const StudentHome = lazy(() => import("./pages/student/StudentHome"));
+const StudentAnnonce = lazy(() => import("./pages/student/StudentAnnonce"));
+const StudentTest = lazy(() => import("./pages/student/StudentTest"));
+const StudentProfile = lazy(() => import("./pages/student/StudentProfile"));
+const StudentResult = lazy(() => import("./pages/student/StudentResult"));
+const TestRoom = lazy(() => import("./pages/student/ExamenSpace/TestRoom"));
+const AdminHome = lazy(() => import("./pages/admin/AdminHome"));
+const AdminGroupe = lazy(() => import("./pages/admin/AdminGroupe"));
+const AdminGroupeEdit = lazy(() => import("./pages/admin/groupe/AdminGroupeEdit"));
+const AdminAccount = lazy(() => import("./pages/admin/AdminAccount"));
+const TeacherHome = lazy(() => import("./pages/teacher/TeacherHome"));
+const TeacherAnnounce = lazy(() => import("./pages/teacher/TeacherAnnounce"));
+const TeacherAnnounceEdit = lazy(() => import("./pages/teacher/annonce/TeacherAnnonceEdit"));
+const TeacherTest = lazy(() => import("./pages/teacher/TeacherTest"));
+const TeacherTestAdd = lazy(() => import("./pages/teacher/test/TeacherTestAdd"));
+const TeacherTestEdit = lazy(() => import("./pages/teacher/test/TeacherTestEdit"));
+const TeacherTestView = lazy(() => import("./pages/teacher/test/TeacherTestView"));
+const TeacherQCMView = lazy(() => import("./pages/teacher/test/TeacherQCMView"));
+const TeacherQuestionEdit = lazy(() => import("./pages/teacher/test/TeacherQuestionEdit"));
+const TeacherCorrection = lazy(() => import("./pages/teacher/TeacherCorrection"));
+const TeacherCorrectionView = lazy(() => import("./pages/teacher/correction/TeacherCorrectionView"));
+const TeacherCorrectionAction = lazy(() => import("./pages/teacher/correction/TeacherCorrectionAction"));
+const TeacherResult = lazy(() => import("./pages/teacher/TeacherResult"));
+const TeacherResultView = lazy(() => import("./pages/teacher/result/TeacherResultView"));
+const TeacherProfile = lazy(() => import("./pages/teacher/TeacherProfile"));
+const AdminHistory = lazy(() => import("./pages/admin/AdminHistory"));
+const AdminHistoryView = lazy(() => import("./pages/admin/history/AdminHistoryView"));
+const AdminResponseView = lazy(() => import("./pages/admin/history/AdminResponseView"));
+const AdminResult = lazy(() => import("./pages/admin/AdminResult"));
+const RoleBasedRoute = lazy(() => import("./routes/RoleBasedRoute"));
+const TeacherTestLaunchedView = lazy(() => import("./pages/teacher/test/TeacherTestLaunchedView"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Unauthorized = lazy(() => import("./pages/Unauthorized"));
 import { USER_ROLE } from "./enum/user.enum";
-import TeacherResultResponseView from "./pages/teacher/result/TeacherResultResponseView";
+const TeacherResultResponseView = lazy(() => import("./pages/teacher/result/TeacherResultResponseView"));
+import { LoadingOutlined } from "@ant-design/icons";
 
 function App() {
   return (
     <Routes>
       {/* Public routes */}
-      <Route index path="/" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/unauthorized" element={<Unauthorized />} />
-      <Route path="*" element={<NotFound />} />
+      <Route index path="/" element={
+        <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+          <LoginPage />
+        </Suspense>
+      } />
+      <Route path="/signup" element={
+        <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+          <SignupPage />
+        </Suspense>
+        } />
+      <Route path="/unauthorized" element={
+        <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+          <Unauthorized />
+        </Suspense>
+        } />
+      <Route path="*" element={
+        <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+          <NotFound />
+        </Suspense>
+        } />
       {/* Student route */}
       <Route
         path="/student"
         element={<RoleBasedRoute allowedRoles={[`${USER_ROLE.ETUDIANT}`]} />}
       >
-        <Route path="home" element={<StudentHome />} />
-        <Route path="announce" element={<StudentAnnonce />} />
-        <Route path="test" element={<StudentTest />} />
-        <Route path="profile" element={<StudentProfile />} />
-        <Route path="result" element={<StudentResult />} />
-        <Route path="test/room/:testId/:tentativeId" element={<TestRoom />} />
+        <Route path="home" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <StudentHome />
+          </Suspense>
+          } />
+        <Route path="announce" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <StudentAnnonce />
+          </Suspense>
+          } />
+        <Route path="test" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <StudentTest />
+          </Suspense>
+          } />
+        <Route path="profile" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <StudentProfile />
+          </Suspense>
+          } />
+        <Route path="result" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <StudentResult />
+          </Suspense>
+          } />
+        <Route path="test/room/:testId/:tentativeId" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TestRoom />
+          </Suspense>
+          } />
       </Route>
       {/* Admin route */}
       <Route
         path="/admin"
         element={<RoleBasedRoute allowedRoles={[`${USER_ROLE.ADMIN}`]} />}
       >
-        <Route path="home" element={<AdminHome />} />
-        <Route path="groupe" element={<AdminGroupe />} />
-        <Route path="history" element={<AdminHistory />} />
-        <Route path="history/view/:id" element={<AdminHistoryView />} />
-        <Route path="result" element={<AdminResult />} />
-        <Route path="response/view/:id" element={<AdminResponseView />} />
-        <Route path="groupe/edit/:id" element={<AdminGroupeEdit />} />
-        <Route path="account" element={<AdminAccount />} />
+        <Route path="home" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminHome />
+          </Suspense>
+          } />
+        <Route path="groupe" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminGroupe />
+          </Suspense>
+          } />
+        <Route path="history" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminHistory />
+          </Suspense>
+          } />
+        <Route path="history/view/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminHistoryView />
+          </Suspense>
+          } />
+        <Route path="result" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminResult />
+          </Suspense>
+          } />
+        <Route path="response/view/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminResponseView />
+          </Suspense>
+          } />
+        <Route path="groupe/edit/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminGroupeEdit />
+          </Suspense>
+          } />
+        <Route path="account" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <AdminAccount />
+          </Suspense>
+          } />
       </Route>
       {/* Teacher route */}
       <Route
         path="/teacher"
         element={<RoleBasedRoute allowedRoles={[`${USER_ROLE.ENSEIGNANT}`]} />}
       >
-        <Route path="home" element={<TeacherHome />} />
-        <Route path="announce" element={<TeacherAnnounce />} />
-        <Route path="announce/edit/:id" element={<TeacherAnnounceEdit />} />
-        <Route path="test" element={<TeacherTest />} />
-        <Route path="test/create" element={<TeacherTestAdd />} />
-        <Route path="test/edit/:id" element={<TeacherTestEdit />} />
-        <Route path="test/view/:id" element={<TeacherTestView />} />
+        <Route path="home" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherHome />
+          </Suspense>
+          } />
+        <Route path="announce" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherAnnounce />
+          </Suspense>
+          } />
+        <Route path="announce/edit/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherAnnounceEdit />
+          </Suspense>
+          } />
+        <Route path="test" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherTest />
+          </Suspense>
+          } />
+        <Route path="test/create" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherTestAdd />
+          </Suspense>
+          } />
+        <Route path="test/edit/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherTestEdit />
+          </Suspense>
+          } />
+        <Route path="test/view/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherTestView />
+          </Suspense>
+          } />
         <Route
           path="test/launched/view/:id"
-          element={<TeacherTestLaunchedView />}
+          element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherTestLaunchedView />
+          </Suspense>
+          }
         />
-        <Route path="qcm/:id" element={<TeacherQCMView />} />
-        <Route path="question/edit/:id" element={<TeacherQuestionEdit />} />
-        <Route path="correction" element={<TeacherCorrection />} />
-        <Route path="correction/view/:id" element={<TeacherCorrectionView />} />
+        <Route path="qcm/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherQCMView />
+          </Suspense>
+          } />
+        <Route path="question/edit/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherQuestionEdit />
+          </Suspense>
+          } />
+        <Route path="correction" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherCorrection />
+          </Suspense>
+          } />
+        <Route path="correction/view/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherCorrectionView />
+          </Suspense>
+          } />
         <Route
           path="correction/action/:testId/:reponseId"
-          element={<TeacherCorrectionAction />}
+          element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherCorrectionAction />
+          </Suspense>
+         }
         />
-        <Route path="result" element={<TeacherResult />} />
-        <Route path="result/view/:id" element={<TeacherResultView />} />
+        <Route path="result" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherResult />
+          </Suspense>
+          } />
+        <Route path="result/view/:id" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherResultView />
+          </Suspense>
+          } />
         <Route
           path="result/response/view/:id"
-          element={<TeacherResultResponseView />}
+          element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherResultResponseView />
+          </Suspense>
+         }
         />
-        <Route path="profile" element={<TeacherProfile />} />
+        <Route path="profile" element={
+          <Suspense fallback={<div className="w-full h-screen flex flex-col justify-center"><LoadingOutlined className="text-7xl w-max m-auto" /></div>}>
+            <TeacherProfile />
+          </Suspense>
+          } />
       </Route>
     </Routes>
   );
